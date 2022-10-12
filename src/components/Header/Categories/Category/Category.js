f53fd27c76d9b9a622bcb19fd816d5e27052c7b6
sawroar <sawroarhossain5@gmail.com>
2022-10-12 22:05:18 +0600

    final touch done

--- src/components/Header/Categories/Category/Category.js
+++ src/components/Header/Categories/Category/Category.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
 const Category = ({ category }) => {
     const { id, logo, name, } = category;
     return (
@@ -12,7 +12,7 @@ const Category = ({ category }) => {
                         <div className="card-body bg-light">
                             <h5 className="card-title "> {name}</h5>
                             <Link to={`/quiz/${id}`}>
-                                <button type="button" className="btn btn-primary">Start Practice Here</button></Link>
+                                <button type="button" className="btn btn-primary">Start Practice Here <span><ArrowRightIcon style={{ height: '30px' }}> </ArrowRightIcon></span></button></Link>
                         </div>
 
                     </div>
--- src/components/Header/Categories/Display/Display.js
+++ src/components/Header/Categories/Display/Display.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
 const Display = () => {
     return (
         <div>
             <div className='text-white p-5 bg-secondary mega-bg rounded'> <h1>The Mega Quiz</h1>
                 <p className='text-white'>Welcome to the Web Development <span className=' text-warning fw-bold fs-2'>Mega Quiz!</span>  Please select your answer and click submit. Question: Question goes here. Questions are taken from W3Schools tutorial.Play this game to review App Development. Fill in the blank: Website: A collection of. web pages on the World Wide Web</p>
-                <button type="button" class="btn btn-info">See More ...</button>
+                <button type="button" class="btn btn-info">See More ... <ArrowRightIcon style={{ height: '30px' }}> </ArrowRightIcon></button>
             </div>
             <img className='mt-5' src="./../../../images/jeshoots-com--2vD8lIhdnw-unsplash.jpg" alt="" />
         </div >
