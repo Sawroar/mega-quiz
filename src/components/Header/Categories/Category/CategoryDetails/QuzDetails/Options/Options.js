d5ab47f639eb656ab48a9e83c970c29bc04ba63d
sawroar <sawroarhossain5@gmail.com>
2022-10-12 15:59:34 +0600

    statistic problem resolved  section completed

--- src/components/Header/Categories/Category/CategoryDetails/QuzDetails/Options/Options.js
+++ src/components/Header/Categories/Category/CategoryDetails/QuzDetails/Options/Options.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Options = ({ option }) => {
-    console.log(option)
+const Options = ({ option, quiz }) => {
+
+    const { correctAnswer } = quiz;
+    const myANs = [correctAnswer];
+    const ans = myANs[0].length
+
+    const [selected, setSelected] = useState();
+
+
+    const handleChange = event => {
+        const myEVen = event.target.value
+        const myArr = [myEVen]
+
+        const ly = myArr[0].length
+        if (ly !== ans) {
+            alert('your anser is correct')
+        }
+        else {
+            alert('anser is incorrect')
+        }
+
+        setSelected(event.target.value)
+
+    };
+
+
     return (
         <div className='text-center'>
-            <input type="checkbox" />
-            <button className='border border-primary mt-2 fw-bold rounded outline-primary p-2 ms-2'>  {option}</button>
+            <input type="radio" value={correctAnswer}
+                checked={selected === correctAnswer} onClick={handleChange} />
+            <button className='border border-primary mt-2 fw-bold rounded outline-primary p-2 ms-2' name="choose"
+
+            >  {option}</button>
         </div>
     );
 };
--- src/components/Header/Categories/Category/CategoryDetails/QuzDetails/QuizDetails.js
+++ src/components/Header/Categories/Category/CategoryDetails/QuzDetails/QuizDetails.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Options from './Options/Options';
 import './QuizDetails.css'
 const QuizDetails = ({ quiz }) => {
+
     const { question, options, } = quiz
     return (
         <div className='contianer  shadow-sm p-3  mt-3 mb-5 bg-body rounded bg-light border-dark'>
             <h3 className='text-center mb-3'>Question:{question.slice(3, question.length - 4)}</h3>
             <div className='container mt-3 options  mt-2 justify-content-around '>
-                {options.map(option => <Options option={option} key={option}></Options>)}
+                {options.map(option => <Options option={option} key={option} quiz={quiz}></Options>)}
 
 
 
